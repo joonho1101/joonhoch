0d97597d9900f0d5237bd301902fc71a7203ef17
Joon Ho Cho <joonho1101@gmail.com>
2012-09-23 15:53:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joonhoch::Application.config.secret_token = '8e4f7ca31c15af4bad881042814eca1e3520d16a8ee65cf905f94a13df9120be9234e00176b97997195ea25226adcd462b797718da68fe9c5d7578134c7c5cde'
